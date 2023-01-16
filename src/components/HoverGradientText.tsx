270b70022fe8052739fc7356d2735a5697b8df37
sujenphea <sujenphea@gmail.com>
2023-01-17 08:53:15 +1300

    fix: add params for hoverGradientText component

--- src/components/HoverGradientText.tsx
+++ src/components/HoverGradientText.tsx
@@ -4,9 +4,18 @@ import { DetailedHTMLProps, HTMLAttributes, useState, useRef, useCallback } from
 import useIntersectionObserver from "../hooks/useIntersectionObserver"
 import { useWindowSize } from "../hooks/useWindowSize"
 
-export const HoverGradientText = (props: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => {
+/**
+ * @param hoverSpotlightRadius Relative to text container in percentage.
+ */
+export const HoverGradientText = (
+  props: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+    nonHoverGradient?: string
+    hoverGradient?: string
+    hoverSpotlightRadius?: number
+  }
+) => {
   // eslint-disable-next-line react/destructuring-assignment
-  const { className, children, ...rest } = props
+  const { className, children, nonHoverGradient, hoverGradient, hoverSpotlightRadius, ...rest } = props
 
   /* --------------------------------- states --------------------------------- */
   const [mouseXPosition, setmouseXPosition] = useState("50%")
@@ -37,71 +46,88 @@ export const HoverGradientText = (props: DetailedHTMLProps<HTMLAttributes<HTMLDi
 
   /* ---------------------------------- main ---------------------------------- */
   return (
-    <div>
-      <div
-        className={`grid ${className}`}
-        ref={containerRef}
-        onMouseMove={handleMove}
-        onMouseEnter={() => {
-          setHovering(true)
+    <div
+      className={`grid ${className}`}
+      ref={containerRef}
+      onMouseMove={handleMove}
+      onMouseEnter={() => {
+        setHovering(true)
+      }}
+      onMouseLeave={() => {
+        setHovering(false)
+      }}
+      {...rest}
+    >
+      {/* linear gradient */}
+      <motion.div
+        animate={{
+          backgroundPosition: "-200% center",
+          transition: {
+            duration: 5,
+            repeat: Infinity,
+            repeatDelay: 0,
+            type: "tween",
+            ease: "linear",
+          },
         }}
-        onMouseLeave={() => {
-          setHovering(false)
+        className="px-2 pt-1" // make sure bg covers text
+        style={{
+          gridArea: "1 / 1",
+          color: "transparent",
+          background:
+            hoverGradient ||
+            "linear-gradient(90deg, rgba(18,194,233,1) 0%, rgba(196,113,237,1) 31%, rgba(246,79,89,1) 70%, rgba(18,194,233,1) 100%)",
+          backgroundClip: "text",
+          height: "100%",
+          width: "100%",
+          backgroundSize: "200%",
         }}
-        {...rest}
       >
-        {/* linear gradient */}
-        <motion.div
-          animate={{
-            backgroundPosition: "-200% center",
-            transition: {
-              duration: 5,
-              repeat: Infinity,
-              repeatDelay: 0,
-              type: "tween",
-              ease: "linear",
-            },
-          }}
-          className="px-2 pt-1" // make sure bg covers text
-          style={{
-            gridArea: "1 / 1",
-            color: "transparent",
-            background:
-              "linear-gradient(90deg, rgba(18,194,233,1) 0%, rgba(196,113,237,1) 31%, rgba(246,79,89,1) 70%, rgba(18,194,233,1) 100%)",
-            backgroundClip: "text",
-            height: "100%",
-            width: "100%",
-            backgroundSize: "200%",
-          }}
-        >
-          {children}
-        </motion.div>
+        {children}
+      </motion.div>
 
-        {/* spotlight effect */}
-        <div
-          className="px-2 pt-1" // make sure bg covers text
-          style={{
-            gridArea: "1 / 1",
-            color: "transparent",
-            backgroundImage: `radial-gradient(circle at ${mouseXPosition} 50%, rgba(0, 0, 0, 0), rgb(0, 0, 0, 1) 50%`,
-            backgroundClip: "text",
-          }}
-        >
-          {children}
-        </div>
-
-        {/* overlay if not hovering */}
-        <div
-          className="px-2 pt-1 transition-opacity duration-500" // make sure bg covers text
-          style={{
-            opacity: hovering ? 0 : 1,
-            gridArea: "1 / 1",
-            color: "black",
-          }}
-        >
-          {children}
-        </div>
+      {/* spotlight effect */}
+      <div
+        className="px-2 pt-1" // make sure bg covers text
+        style={{
+          gridArea: "1 / 1",
+          color: "transparent",
+          backgroundImage: `radial-gradient(circle at ${mouseXPosition} ${
+            100 - (hoverSpotlightRadius || 50)
+          }%, rgba(0, 0, 0, 0), rgb(0, 0, 0, 1) ${hoverSpotlightRadius || 50}%`,
+          backgroundClip: "text",
+        }}
+      >
+        {children}
       </div>
+
+      {/* overlay if not hovering */}
+      <motion.div
+        animate={{
+          backgroundPosition: "-200% center",
+          transition: {
+            duration: 5,
+            repeat: Infinity,
+            repeatDelay: 0,
+            type: "tween",
+            ease: "linear",
+          },
+        }}
+        className="px-2 pt-1 transition-opacity duration-300" // make sure bg covers text
+        style={{
+          opacity: hovering ? 0 : 1,
+          gridArea: "1 / 1",
+          color: "transparent",
+          background:
+            nonHoverGradient || "linear-gradient(to right, #747474 0, #1f1f1f 40%, #1f1f1f 60%, #747474 100%)",
+          backgroundClip: "text",
+          height: "100%",
+          width: "100%",
+          backgroundSize: "200%",
+        }}
+      >
+        {children}
+      </motion.div>
     </div>
   )
 }
