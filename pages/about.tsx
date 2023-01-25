bfb72be01d9241b4ae14c04c89719fa8b2535797
sujenphea <sujenphea@gmail.com>
2023-01-25 14:07:43 +1300

    fix: populate about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,36 +9,36 @@ const timelineItems: {
   id: number
   role: string
   company: string
-  description: string
+  description: string[]
   startDate: string
   endDate: string
 }[] = [
   {
     id: 0,
-    role: "test",
-    company: "test",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus vel metus ac vestibulum. Suspendisse a dolor diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In ut egestas ante. Pellentesque ultricies neque quis condimentum dictum",
-    startDate: "test",
-    endDate: "test",
+    role: "Bachelor's degree, Computer Science",
+    company: "University of Auckland",
+    description: [],
+    startDate: "June 2020",
+    endDate: "December 2022",
   },
   {
     id: 1,
-    role: "test",
-    company: "test",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus vel metus ac vestibulum. Suspendisse a dolor diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In ut egestas ante. Pellentesque ultricies neque quis condimentum dictum",
-    startDate: "test",
-    endDate: "test",
+    role: "Developer",
+    company: "Umajin Inc.",
+    description: [
+      "Developed features for the in-house mobile application design and deployment app.",
+      "Co-developed a software platform using the in-house software language.",
+    ],
+    startDate: "November 2021",
+    endDate: "June 2022",
   },
   {
     id: 2,
-    role: "test",
-    company: "test",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus vel metus ac vestibulum. Suspendisse a dolor diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In ut egestas ante. Pellentesque ultricies neque quis condimentum dictum",
-    startDate: "test",
-    endDate: "test",
+    role: "Freelance",
+    company: "Web development",
+    description: ["Collaborated closely with designers to develop interactive websites for clients."],
+    startDate: "October 2022",
+    endDate: "Current",
   },
 ]
 
@@ -50,10 +50,11 @@ const technologyItems: {
   name: string
   skillLevel: number
 }[] = [
-  { id: 0, name: "React", skillLevel: 1 },
-  { id: 1, name: "React", skillLevel: 0.9 },
-  { id: 2, name: "React", skillLevel: 0.6 },
-  { id: 3, name: "React", skillLevel: 0.3 },
+  { id: 0, name: "NextJS", skillLevel: 1 },
+  { id: 1, name: "Typescript", skillLevel: 1 },
+  { id: 2, name: "ThreeJS", skillLevel: 0.9 },
+  { id: 3, name: "Python", skillLevel: 0.9 },
+  { id: 4, name: "Swift", skillLevel: 0.8 },
 ]
 
 /* -------------------------------------------------------------------------- */
@@ -97,9 +98,15 @@ export default function About() {
         {/* decoration  */}
         <div className="absolute top-0 flex -translate-y-full flex-col">
           {/* content */}
-          <div className="relative -left-8 flex items-center gap-8">
+          <div className="relative -translate-x-1/2">
             {/* circle */}
-            <div className="h-16 w-16 rounded-full bg-slate-300" />
+            <svg viewBox="2 3 27 36" className="w-6 rotate-180">
+              <path
+                className="fill-slate-300"
+                strokeWidth="3"
+                d="M15 3 Q16.5 6.8 25 18 A12.8 12.8 0 1 1 5 18 Q13.5 6.8 15 3z"
+              />
+            </svg>
           </div>
 
           {/* vertical line */}
@@ -107,9 +114,9 @@ export default function About() {
         </div>
 
         {/* content */}
-        {timelineItems.map((item) => {
+        {timelineItems.reverse().map((item) => {
           return (
-            <div key={item.id} className="border-l border-slate-300">
+            <div key={`timline ${item.id}`} className="border-l border-slate-300">
               {/* date */}
               <div className="flex items-center gap-5">
                 {/* ball */}
@@ -129,12 +136,34 @@ export default function About() {
                 <div className="text-lg font-medium uppercase">{item.role}</div>
 
                 {/* company */}
-                <div className="mt-1">
-                  <div className="text-sm lowercase opacity-75">{item.company}</div>
-                </div>
+                {item.company.length > 0 && (
+                  <div className="mt-1">
+                    <div className="text-sm opacity-75">{item.company}</div>
+                  </div>
+                )}
 
                 {/* description */}
-                <div className="mt-3 max-w-[80%]">{item.description}</div>
+                <div className="mt-3 max-w-[80%] font-light">
+                  {item.description.length > 1
+                    ? // bulletpoint
+                      item.description.map((bulletpoint, id) => {
+                        return (
+                          <div
+                            // eslint-disable-next-line react/no-array-index-key
+                            key={`timeline ${item.id}, ${id}`}
+                            className="relative flex gap-2"
+                          >
+                            {/* bullet */}
+                            <div>•</div>
+
+                            {/* text */}
+                            <div>{bulletpoint}</div>
+                          </div>
+                        )
+                      })
+                    : // paragraph
+                      item.description}
+                </div>
               </div>
             </div>
           )
@@ -156,25 +185,25 @@ export default function About() {
           </div>
 
           {/* content */}
-          <div className="mt-3">
+          <div className="mt-8">
             <div className="flex flex-col gap-7">
               {technologyItems.map((item) => {
                 return (
-                  <div key={item.id}>
+                  <div key={`technologies ${item.id}`}>
                     {/* title */}
                     <div className="text-sm font-medium uppercase">{item.name}</div>
 
                     {/* non interactive slider */}
-                    <div
-                      className="h-[2px] w-full"
-                      style={{
-                        backgroundImage: `linear-gradient(to right, ${brandColor} ${item.skillLevel * 100}%, white ${
-                          (item.skillLevel + 0.001) * 100
-                        }%)`,
-                      }}
-                    />
-
-                    {/*  */}
+                    <div className="mt-3">
+                      <div
+                        className="h-[2px] w-full"
+                        style={{
+                          backgroundImage: `linear-gradient(to right, ${brandColor} ${item.skillLevel * 100}%, white ${
+                            (item.skillLevel + 0.001) * 100
+                          }%)`,
+                        }}
+                      />
+                    </div>
                   </div>
                 )
               })}
@@ -189,10 +218,10 @@ export default function About() {
   return (
     <FullPageDisplay>
       {/* button */}
-      <div className="mt-1">
+      <div className="mt-3">
         <div className="flex justify-center">
-          <div className="rounded-xl bg-slate-600 p-4">
-            <IconX className="h-5 w-5 text-white" />
+          <div className="rounded-full bg-gray-50 p-4">
+            <IconX className="h-5 w-5 text-slate-500" />
           </div>
         </div>
       </div>
